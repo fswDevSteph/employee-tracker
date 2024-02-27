3b31a77c803585682e5fc74bb810de172ec3d317
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-02-27 11:46:13 -0400

    first 'then" requirements of user story conmplete

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ const question = [
     type: 'list',
     name: 'doWhat',
     message: 'What would you like to do?',
-    choices: ['View All Departments', 'View All Roles'],
+    choices: [
+      'View All Departments',
+      'View All Roles',
+      'View All Employees',
+      'Add A Department',
+      'Add A Role',
+      'Add A Employee',
+      'Update an Employee Role',
+    ],
   },
 ];
 
